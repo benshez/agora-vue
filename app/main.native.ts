8054184ca2e290f4d764bcc32498eb1b1cf03482
Ben van Heerden <benshez@gmail.com>
2019-01-07 07:37:36 +1000

    Fix Native app.

--- app/main.native.ts
+++ app/main.native.ts
@@ -9,17 +9,17 @@ import './styles';
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = __TNS_ENV__ === 'production';
-Vue.prototype.$isAndroid = platformModule.isAndroid;
-Vue.prototype.$isIOS = platformModule.isIOS;
+//Vue.prototype.$isAndroid = platformModule.isAndroid;
+//Vue.prototype.$isIOS = platformModule.isIOS;
 
 new Vue({
 	computed: {
-		pageClasses: function () {
-			return {
-				'platform-ios': platformModule.isIOS,
-				'platform-android': platformModule.isAndroid
-			};
-		}
+		//	pageClasses: function () {
+		//		return {
+		//			'platform-ios': platformModule.isIOS,
+		//			'platform-android': platformModule.isAndroid
+		//		};
+		//	}
 	},
 	render: (h) => h('frame', [h(App)])
 }).$start();
